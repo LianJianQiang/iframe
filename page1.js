99d5df050f0d747dc4b7266ddf1c18022a8c5d9a
lianjianqiang <632920053@qq.com>
2017-07-31 14:42:19 +0800

    iframe跨域，postMessage

---
+++ page1.js
@@ -0,0 +1,22 @@
+var http=require('http');
+var fs=require('fs');
+
+http.createServer(function (req, res) {
+
+    var url=req.url;
+    var file=__dirname+url;
+    console.log(__dirname)
+    fs.readFile(file,function (err,data) {
+
+        if(err){
+            res.writeHeader(404,{'content-type':'text/html;charset="utf-8"'});
+            res.write('<h1>404</h1>')
+            res.end();
+        }else{
+            res.writeHeader(200,{'content-type':'text/html;charset="utf-8"'});
+            res.write(data);
+            res.end()
+        }
+    })
+
+}).listen('4100')
